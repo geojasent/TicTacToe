c3c0c27b6a5f6a45c4bef5a48a4d1204d3196b1d
Jason Wong <wongjason415@gmail.com>
2022-04-02 18:54:55 -0700

    added win conditions, needs win condition logic

--- script.js
+++ script.js
@@ -1,19 +1,47 @@
 const game = (() => {
     
+    var player1;
+    var player2;
+    var computer;
+    var tileId;
+    
     var gameboard = (() => {
         var board = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
         return {board};
     })();
 
-    //screen to choose number of players
-    const playerFactory = ((name, symbol, type) => {
-        console.log("playerfactory");
-    });
-    //displays and updates the gameboard
     const displayController = (() => {
-        playerFactory()
-        //display board
-        //add event listener for only ony click to each tile
+        const initializePlayers = (() => {
+            var numPlayers = document.getElementsByTagName("button");
+            Array.from(numPlayers).forEach(function(element) {
+                element.addEventListener("click", playerAssign);
+            });
+            
+            function playerAssign() {
+                var buttonPlayer = event.target.id;
+
+                function playerFactory(buttonId, shape) {
+                    var player = {};
+                    player.buttonId = buttonId;
+                    player.shape = shape;
+                    return player;
+                };
+
+                switch (buttonPlayer) {
+                    case "1player":
+                        player1 = playerFactory("player1", "X");
+                        computer = playerFactory("computer", "O")
+                        break;
+                    case "2player":
+                        player1 = playerFactory("player1", "X");
+                        player2 = playerFactory("player2", "O");
+                        break;
+                    }
+                    return (player1, player2);
+                }
+        })();
+
+        //add event listener for only one click to each tile
         const initializeBoard = (() => {
             var addClick = document.getElementsByClassName("boardTile");
             Array.from(addClick).forEach(function(element) {
@@ -21,11 +49,11 @@ const game = (() => {
             });
         })();
         
-        //when this is called get the clicked tile
         function getTile() {
-            var tileId = event.target.id;
+            tileId = event.target.id;
             var tile = document.getElementById(tileId);
             removeClick(tile);
+            return tile;
         };
 
         function removeClick(tile) {
@@ -36,15 +64,44 @@ const game = (() => {
         //make this work for each player or computer
         //conditional for 2 players
         function addShape(tile) {
-            tile.innerHTML = "tileTest"
-            console.log("addShape");
-        }        //on click function to add shape to tile
-    })();
+            var tileNumber = tile.dataset.number;
+            if (player1.shape === "X") {
+                tile.innerHTML = player1.shape;
+                gameboard.board[tileNumber] = player1.shape
+                player1.shape = "O";
+            } else {
+                tile.innerHTML = player1.shape;
+                gameboard.board[tileNumber] = player1.shape
+                player1.shape = "X";
+            }
+            checkWinModule.checkWin();
+        };
 
-    //check tilies for win condition
+        const checkWinModule = (() => {
+            const winConditions = [
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8],
+                [0, 3, 6],
+                [1, 4, 7],
+                [2, 5, 8],
+                [0, 4, 8],
+                [2, 4, 6],
+            ];
+
+            function checkWin() {
+                console.log(gameboard.board)
+                console.log("test");
+
+            };
+            return {checkWin};
+            //iterate through board and check for x or o
+            //if x or o 3 in a show box with win
+        })();
+    })();
+    
     return {
         gameboard,
-        playerFactory,
         displayController
     }
 })();
